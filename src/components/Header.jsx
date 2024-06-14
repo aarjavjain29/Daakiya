a27678b00c8fbbb65292fb6667496ec065ab9af5
Aarjav <aarjavjain2901@gmail.com>
2024-06-14 23:43:19 +0530

    navbar-updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
-                <a className="navbar-brand" href="/">PostMaster</a>
+                <a className="navbar-brand" href="/">Daakiya</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
                     aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
